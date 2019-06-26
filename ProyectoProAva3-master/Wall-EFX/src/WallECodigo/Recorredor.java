e89674d1296ad609666808c44e14f812af9be6a3
andresjvs <52140962+andresjvs@users.noreply.github.com>
2019-06-26 18:58:45 -0300

    Clase serializable, calculo del tiempo de ejecucion.
    Se agrego la clase serializable con sus métodos.
    Se agrego el calculo de el tiempo de ejecución.

---
+++ ProyectoProAva3-master/Wall-EFX/src/WallECodigo/Recorredor.java
@@ -0,0 +1,16 @@
+package WallECodigo;
+
+import java.util.ArrayList;
+
+public class Recorredor {
+
+    private static int[][] direcciones = {{0,1},{1,0},{0,-1},{-1,0}};
+
+    public ArrayList<Posicion> buscarRuta(){
+        //return;
+    }
+
+    private boolean comprobar(Recinto recinto, int fila, int columna, ArrayList<Posicion> camino){
+        return true;
+    }
+}
