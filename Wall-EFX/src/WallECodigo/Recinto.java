55e5e16cfb31688c81e7ef6f49982efdfe08174c
Carolyn1998 <carolyn.anahin@gmail.com>
2019-07-06 21:27:33 -0400

    Menu despegable implementado
    Se muestran las rutas en el menu, pero solo desde walle hasta la planta, falta condicion para que muestre hasta eva.

--- Wall-EFX/src/WallECodigo/Recinto.java
+++ Wall-EFX/src/WallECodigo/Recinto.java
@@ -12,25 +12,27 @@ public class Recinto {
 
     private int limiteColumnas;
 
-    private int[][] recintoCompleto = new int [50][50];
+    private int[][] recintoCompleto = new int [20][30];
 
     private Posicion pActual;
 
-    private int sizeFilas;
-
-    private int sizeColumnas;
-
     private char orientacion;
 
+    public Recinto(int limiteFilas, int limiteColumnas, char orientacion) {
+        this.limiteFilas = limiteFilas;
+        this.limiteColumnas = limiteColumnas;
+        this.orientacion = orientacion;
+    }
+
+    public Recinto(){        
+    }
     
     /***
      * Es el metodo que se encarga de construir la matriz que conforma al recinto en el que se mueve Wall-E
      * @param valor Es el valor que poseerá la celda en la matriz, ya sea un 0 (Nada) o un 1 (Si es una bomba)
      */
     public void crearRecinto(int Filas, int Columnas, int valor){
-        recintoCompleto[Filas][Columnas] = valor;
-        sizeFilas = Filas;
-        sizeColumnas = Columnas;
+        recintoCompleto[Filas][Columnas] = valor;        
     }
 
     /***
@@ -57,7 +59,7 @@ public class Recinto {
         return (recinto.getRecintoCompleto()[fila][columna] == 5);
     }
 
-    boolean esDestinoPlanta(int fila, int columna){
+    public boolean esDestinoPlanta(int fila, int columna){
         return (returnWallE().getpDPlanta().getpFila() == fila &&
                 returnWallE().getpDPlanta().getpColumna() == columna);
     }
@@ -131,4 +133,5 @@ public class Recinto {
     public void setRecintoCompleto(int[][] recintoCompleto) {
         this.recintoCompleto = recintoCompleto;
     }
+    
 }
